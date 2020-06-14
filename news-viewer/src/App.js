03c95566b7c23ee6ea33b1c6dbb085691e64fe1c
gooddevuser <gooddevuser@naver.com>
2020-06-14 22:50:26 +0900

    news-viewer

---
+++ news-viewer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './components/NewsPage';
+
+function App() {
+  return (
+    
+    <>
+      <Route path="/:category?" component={ NewsPage } />      
+    </>
+  );
+}
+
+export default App;
